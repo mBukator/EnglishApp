5defe7c016a0a6d1e17d6d7ed35d552388fb13bd
pudj1 <dxxdimaxxd228@gmail.com>
2023-02-05 23:30:44 +0200

    lab 2 done

---
+++ eng-app/src/users/interfaces/user.interface.ts
@@ -0,0 +1,8 @@
+export class User {
+    _id: string;
+    name: string;
+    age: number;
+    isActive: boolean;
+    email: string;
+    phone: string;
+}
--- eng-app/src/users/user.service.ts
+++ eng-app/src/users/user.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { users } from '../moks';
+
+import { Injectable,UseFilters } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { users } from 'src/moks';
 import { CreateUserDTO } from './dto/createUser.dto';
+import { NoDataExeptionFilter } from './user.filter';
+import { NoDataExeption } from './user.exeptions';
 
 @Injectable()
 export class UserService {
+    private readonly users: User[] = users;
+    
+    @UseFilters(new NoDataExeptionFilter())
     getUsers() {
-        return users;
+        if(this.users == null) {
+            throw new NoDataExeption();
+        }
+        return this.users;
     }
+
     async createUser(createUserDTO: CreateUserDTO) {
         return createUserDTO;
     }
 }
+
