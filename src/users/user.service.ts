42aa394805f7ff74732b4caca24eedac19dd87f5
pudj1 <dxxdimaxxd228@gmail.com>
2023-02-06 15:21:58 +0200

    createUser with moks

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable,UseFilters } from '@nestjs/common';
 import { User } from './interfaces/user.interface';
-//import { users } from 'src/moks';
+import { users } from 'src/moks';
 import { CreateUserDTO } from './dto/createUser.dto';
 import { NoDataExeptionFilter } from './user.filter';
 import { NoDataExeption } from './user.exeptions';
@@ -9,7 +9,7 @@ import { UserEntity } from './user.entitiy';
 
 @Injectable()
 export class UserService {
-    private users: User[] = [];
+    private users: User[] = users;
     
     @UseFilters(new NoDataExeptionFilter())
     getUsers() {
@@ -21,10 +21,9 @@ export class UserService {
 
     async createUser(createUserDTO: CreateUserDTO) {
         //const newUser = new UserEntity();
-        const newUser = new User();
-        Object.assign(newUser, createUserDTO)
-        console.log('newUser', newUser);
-        this.users.push(newUser);
+        //Object.assign(newUser, createUserDTO)
+        console.log('newUser', createUserDTO);
+        this.users.push(createUserDTO);
         return createUserDTO;
     }
 }
