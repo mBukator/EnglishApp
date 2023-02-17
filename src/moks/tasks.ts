ba035fb8d46035f516bba0838ac658f37eb0a576
pudj1 <dxxdimaxxd228@gmail.com>
2023-02-17 09:13:53 +0200

    moks update

--- src/moks/tasks.ts
+++ src/moks/tasks.ts
@@ -1,49 +1,49 @@
 export var tasks = [
     {
         "_id": 1,
-        "creatorId": "Garner Johns",
-        "name": "26",
-        "description": "male",
+        "creatorId": "3",
+        "name": "English1",
+        "description": "something...",
         "membersId": [1,2,3],
     },
     {
 
         "_id": 2,
-        "creatorId": "Garner Johns",
-        "name": "26",
-        "description": "male",
+        "creatorId": "2",
+        "name": "English2",
+        "description": "something...",
         "membersId": [1,3]
     
     },
     {
         "_id": 3,
-        "creatorId": "Garner Johns",
-        "name": "26",
-        "description": "male",
+        "creatorId": "4",
+        "name": "English3",
+        "description": "something...",
         "membersId": [1,2,3],
     },
     {
 
         "_id": 4,
-        "creatorId": "Garner Johns",
-        "name": "26",
-        "description": "male",
+        "creatorId": "5",
+        "name": "English4",
+        "description": "something...",
         "membersId": [1,3]
     
     },
     {
         "_id": 5,
-        "creatorId": "Garner Johns",
-        "name": "26",
-        "description": "male",
+        "creatorId": "6",
+        "name": "English5",
+        "description": "something...",
         "membersId": [1,2,3],
     },
     {
 
         "_id": 6,
-        "creatorId": "Garner Johns",
-        "name": "26",
-        "description": "male",
+        "creatorId": "7",
+        "name": "English6",
+        "description": "something...",
         "membersId": [1,3]
     
     }
